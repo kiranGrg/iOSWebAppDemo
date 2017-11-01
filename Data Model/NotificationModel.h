c7efc8fea732f72dd9805e9bdeeffe60d94d0f2c
Kiran Gurung <kgrgtamu@gmail.com>
2017-11-01 17:02:18 +1100

    Update NotificationModel.h

--- Data Model/NotificationModel.h
+++ Data Model/NotificationModel.h
@@ -2,8 +2,7 @@
 //  NotificationModel.h
 //  HottieHunterDemo
 //
-//  Created by Padam Krishna Prajapati on 12/03/14.
-//  Copyright (c) 2014 Ujjwal Shrestha. All rights reserved.
+//  Copyright (c) 2014 DesignOffshore. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
